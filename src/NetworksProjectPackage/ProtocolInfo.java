f346bca1c4f3eac7fd8df16a987fde5173cbb6fa
MrAlwaysRight <wsylafa@gmail.com>
2013-04-17 19:16:10 -0400

    Updated ProtocolInfo

--- src/NetworksProjectPackage/ProtocolInfo.java
+++ src/NetworksProjectPackage/ProtocolInfo.java
@@ -8,71 +8,31 @@ package NetworksProjectPackage;
  *
  * @author This PC
  */
-public class ProtocolInfo {
+interface ProtocolInfo {
+    public final char[] PROTOCOL_HEADER = {'C','S','3','0','5','T','S','P'};
+    public final int MAJOR_VERSION_NUMBER = 1;
+    public final int MINOR_VERSION_NUMBER = 0;
+    public final int TYPE_ERROR = 1;
+    public final int TYPE_REQUEST = 2;
+    public final int TYPE_UNICAST = 3;
+    public final int TYPE_UNICAST_WITH_SERVER_INFO = 4;
+    public final int TYPE_MULTICAST = 5;
+
+    public static final int DEFAULT_CLIENT_LISTEN_PORT_NUMBER = 4444;
+    public static final int DEFAULT_SERVER_LISTEN_PORT_NUMBER = 5555;
+    public static final int DEFAULT_SESSION_SERVER_PORT_NUMBER = 6666;
+    public static final String DEFAULT_SESSION_SERVER_ADDRESS = "localhost";
     
-    private final char[] PROTOCOL_HEADER = {'C','S','3','0','5','T','S','P'};
-    private final int MAJOR_VERSION_NUMBER = 1;
-    private final int MINOR_VERSION_NUMBER = 0;
-    private final int TYPE_REQUEST = 1;
-    private final int TYPE_RESPONSE = 2;
-    private final int TYPE_ERROR = 3;
-    private final int TYPE_UNICAST = 4;
-    private final int TYPE_MULTICAST = 5;
-    
-    private final byte[] RESPONSE_PACKET = { (byte) PROTOCOL_HEADER[0], 
-                                                    (byte) PROTOCOL_HEADER[1], 
-                                                    (byte) PROTOCOL_HEADER[2],
-                                                    (byte) PROTOCOL_HEADER[3], 
-                                                    (byte) PROTOCOL_HEADER[4], 
-                                                    (byte) PROTOCOL_HEADER[5], 
-                                                    (byte) PROTOCOL_HEADER[6], 
-                                                    (byte) PROTOCOL_HEADER[7],
-                                                    MAJOR_VERSION_NUMBER, 
-                                                    MINOR_VERSION_NUMBER, 
-                                                    (byte)(TYPE_RESPONSE>>8), 
-                                                    (byte)TYPE_RESPONSE, 	
-                                                    0, 
-                                                    8};
-    
-    public ProtocolInfo()
-    {
-        
-    }
-    
-    public char[] getProtocolHeader()
-    {
-        return this.PROTOCOL_HEADER;
-    }
-    
-    public char getProtocolHeaderElement(int i)
-    {
-        return this.PROTOCOL_HEADER[i];
-    }
-    
-    public int getMajorVersionNumber()
-    {
-        return this.MAJOR_VERSION_NUMBER;
-    }
-    
-    public int getMinorVersionNumber()
-    {
-        return this.MINOR_VERSION_NUMBER;
-    }
-    
-    public int getTypeError()
-    {
-        return this.TYPE_ERROR;
-    }
-    
-    public int getTypeReqeust()
-    {
-        return this.TYPE_REQUEST;
-    }
-    
-    public int getTypeUnicast()
-    {
-        return this.TYPE_UNICAST;
-    }
-    
-    
+    public final byte[] REQUEST_PACKET_WIHTOUT_LENGTH_DATA = { (byte) PROTOCOL_HEADER[0], 
+                                                                (byte) PROTOCOL_HEADER[1], 
+                                                                (byte) PROTOCOL_HEADER[2],
+                                                                (byte) PROTOCOL_HEADER[3], 
+                                                                (byte) PROTOCOL_HEADER[4], 
+                                                                (byte) PROTOCOL_HEADER[5], 
+                                                                (byte) PROTOCOL_HEADER[6], 
+                                                                (byte) PROTOCOL_HEADER[7],
+                                                                (byte)MAJOR_VERSION_NUMBER, 
+                                                                (byte)MINOR_VERSION_NUMBER, 
+                                                                (byte)(TYPE_REQUEST >> 8), 
+                                                                (byte)TYPE_REQUEST};
 }
