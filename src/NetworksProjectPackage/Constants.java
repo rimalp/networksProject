a15a8fe4273ac8145f0ad27a8342d3a319e70d46
Drew Jeffrey <jeffreyd@lafayette.edu>
2013-04-17 01:04:56 -0400

    Began work on the client GUI controller, added file for constants

---
+++ src/NetworksProjectPackage/Constants.java
@@ -0,0 +1,10 @@
+package NetworksProjectPackage;
+
+interface Constants
+{
+public static final int PLAYER1 = 10;
+public static final int PLAYER1BALL = 11;
+public static final int PLAYER2 = 20;
+public static final int PLAYER2BALL = 21;
+}
+
